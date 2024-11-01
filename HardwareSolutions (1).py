4e032032648e29602e7625bec003777613a62d8f
Thomas Verrill <121411236+thomasverrill@users.noreply.github.com>
2024-11-01 16:24:25 -0400

    Add files via upload

---
+++ HardwareSolutions (1).py
@@ -0,0 +1,25 @@
+from qutip import *
+
+# Note: it is OK if object/function names from qutip are not 
+# highlighted and underlined (e.g. "qutip" or "Qobj"). However, 
+# it is recommended you test your solutions in a Jupyter notebook 
+# before submitting
+
+class HardwareSolutions:
+    def __init__():
+        pass
+
+    def hardware_1(omega:float) -> Qobj:
+        pass
+
+    def hardware_2(Omega_R,omega,omega_drive) -> Qobj:
+        pass
+
+    def hardware_3(Omega_R:float,omega:float,omega_drive:float,t:float) -> float:
+        pass
+
+    def hardware_4(psi_n:float,psi_n_1:float,E_1:float,q:float) -> float:
+        pass
+
+    def hardware_5(Omega_R:float,delta:float) -> float:
+        pass
