d211aeb9c411522e65661405d68127677536828d
vcaipu <113256651+vcaipu@users.noreply.github.com>
2024-11-02 11:43:32 -0400

    Add files via upload

---
+++ HardwareSubmission.py
@@ -0,0 +1,26 @@
+from qutip import *
+from typing import List
+
+# Note: it is OK if object/function names from qutip are not 
+# highlighted and underlined (e.g. "qutip" or "Qobj"). However, 
+# it is recommended you test your solutions in a Jupyter notebook 
+# before submitting
+
+class HardwareSolutions:
+    def __init__():
+        pass
+
+    def hardware_1(self,omega:float) -> Qobj:
+        pass
+
+    def hardware_2(self,Omega_R:float,omega:float,omega_drive:float,t:float) -> Qobj:
+        pass
+
+    def hardware_3(self,Omega_R:float,omega:float,omega_drive:float,t:List[float]) -> float:
+        pass
+
+    def hardware_4(self,psi_n:float,psi_n_1:float,E_1:float,q:float) -> float:
+        pass
+
+    def hardware_5(self,Omega_R:float,delta:float) -> float:
+        pass
